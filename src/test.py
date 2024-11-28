216482de57f5ff3f708ea1ca998421caacc9520a
Truong ITT <hoduytruong280220@gmail.com>
2024-11-28 11:13:16 +0700

    feat:v2 vnauto normalization VietNamese

--- src/test.py
+++ src/test.py
@@ -2,10 +2,10 @@ import time
 from vnauto import get_vnauto_instance  
 
 def test_normalization():
-    vinorm = get_vnauto_instance()
+    vnauto = get_vnauto_instance()
     text = "bchbp Hai £ 2 mm mik ko bít mai có đi học k, mà chắc là oki! bùl 19/10/2004"
     start_time = time.time()
-    result = vinorm.normalize(text)
+    result = vnauto.normalize(text)
     end_time = time.time()
     print("Kết quả chuẩn hóa văn bản:", result)
     print(f"Thời gian hoạt động: {end_time - start_time:.6f} giây")
